bf431b3d2d507fec6cbcc94698c6bd1fe13eaceb
J/ <55988027+jkerai1@users.noreply.github.com>
2022-03-03 16:41:16 +0000

    Update q2D-getTagsscript.sh

--- q2D-getTagsscript.sh
+++ q2D-getTagsscript.sh
@@ -4,8 +4,6 @@
 #$ -l rmem=16G #number of mem
 #$ -o q2D-getTagsoutput.txt #This is where your output and errors are logged.
 #$ -j y # normal and error outputs into a single file (the file above)
-#$ -M jkerai1@shef.ac.uk #Notify you by email, remove this line if you don't like
-#$ -m bea #Email you when it finished or aborted
 #$ -cwd # Run job from current directory
 
 module load apps/java/jdk1.8.0_102/binary
