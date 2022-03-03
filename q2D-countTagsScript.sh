5f462e9fd07aead6aead7febece515ed2058a9d6
J/ <55988027+jkerai1@users.noreply.github.com>
2022-03-03 16:41:02 +0000

    Update q2D-countTagsScript.sh

--- q2D-countTagsScript.sh
+++ q2D-countTagsScript.sh
@@ -4,8 +4,6 @@
 #$ -l rmem=4G #number of mem
 #$ -o Assignment_q1.txt #This is where your output and errors are logged.
 #$ -j y # normal and error outputs into a single file (the file above)
-#$ -M jkerai1@shef.ac.uk #Notify you by email, remove this line if you don't like
-#$ -m bea #Email you when it finished or aborted
 #$ -cwd # Run job from current directory
 
 module load apps/java/jdk1.8.0_102/binary
