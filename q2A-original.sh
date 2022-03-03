aae60938a8c369e227f9c72ac796e374e04d26f6
J/ <55988027+jkerai1@users.noreply.github.com>
2022-03-03 16:39:43 +0000

    Update q2A-original.sh

--- q2A-original.sh
+++ q2A-original.sh
@@ -4,8 +4,6 @@
 #$ -l rmem=16G #number of mem
 #$ -o Assignment_q2_orginal.txt #This is where your output and errors are logged.
 #$ -j y # normal and error outputs into a single file (the file above)
-#$ -M jkerai1@shef.ac.uk #Notify you by email, remove this line if you don't like
-#$ -m bea #Email for begun end and aborted
 #$ -cwd # Run job from current directory
 
 module load apps/java/jdk1.8.0_102/binary
