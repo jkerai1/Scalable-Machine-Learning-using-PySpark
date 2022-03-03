a4d5691ffb3cf3b05a548586cfc9180add83c09c
J/ <55988027+jkerai1@users.noreply.github.com>
2022-03-03 16:41:31 +0000

    Update q2b.sh

--- q2b.sh
+++ q2b.sh
@@ -4,8 +4,6 @@
 #$ -l rmem=2G #number of mem
 #$ -o COM6012_Lab1.output #This is where your output and errors are logged.
 #$ -j y # normal and error outputs into a single file (the file above)
-#$ -M jkerai1@shef.ac.uk #Notify you by email, remove this line if you don't like
-#$ -m ea #Email you when it finished or aborted
 #$ -cwd # Run job from current directory
 
 module load apps/java/jdk1.8.0_102/binary
